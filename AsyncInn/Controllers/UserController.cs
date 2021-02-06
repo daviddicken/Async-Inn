ceaf87069b1c3d75ec1872c53907619b9308c9f9
daviddicken <davidfromseattle@gmail.com>
2021-02-06 13:24:10 -0800

    finished lab 18

---
+++ AsyncInn/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using AsyncInn.Models.API;
+using AsyncInn.Models.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService userService;
+
+        public UserController(IUserService service)
+        {
+            userService = service;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterUser data)
+        {
+            var User = await userService.Register(data, this.ModelState);
+            
+            if (ModelState.IsValid) return User;
+            return BadRequest(new ValidationProblemDetails(ModelState));
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDTO>> Register(LoginData data)
+        {
+            var user = await userService.Authenticate(data.Username, data.Password);
+            if (user != null) return user;
+            return Unauthorized();
+        }
+    }
+}
---
+++ AsyncInn/Models/Interfaces/Services/IdentityUserService.cs
@@ -0,0 +1,66 @@
+﻿using AsyncInn.Models.API;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces.Services
+{
+    public class IdentityUserService : IUserService
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public IdentityUserService(UserManager<ApplicationUser> manager)
+        {
+            userManager = manager;
+        }
+        public async Task<UserDTO> Register(RegisterUser data, ModelStateDictionary modelState)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = data.Username,
+                Email = data.Email,
+                PhoneNumber = data.Phone
+            };
+
+            var result = await userManager.CreateAsync(user, data.Password);
+
+            if (result.Succeeded)
+            {
+                return new UserDTO
+                {
+                    Id = user.Id,
+                    Username = user.UserName
+                };
+            }
+
+            foreach(var error in result.Errors)
+            {
+                var errorKey = 
+                    error.Code.Contains("Email") ? nameof(data.Email) :
+                    error.Code.Contains("Password") ? nameof(data.Password) :
+                    error.Code.Contains("UserName") ? nameof(data.Username) : "";
+
+                modelState.AddModelError(errorKey, error.Description);
+            }
+            return null;
+        }
+
+        public async Task<UserDTO> Authenticate(string userName, string password)
+        {
+            var user = await userManager.FindByNameAsync(userName);
+
+            if(await userManager.CheckPasswordAsync(user, password))
+            {
+                return new UserDTO
+                {
+                    Id = user.Id,
+                    Username = user.UserName
+                };
+            }
+            return null;
+        }
+    }
+}
